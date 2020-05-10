6ce27f8a149e4af0d3ea284b23b778cd5452e6a0
kgarashchuk <wisepinkbird@gmail.com>
2020-05-10 20:12:36 +0300

    added models

--- src/main/java/com/bookingservice/Main.java
+++ src/main/java/com/bookingservice/Main.java
@@ -2,7 +2,6 @@ package com.bookingservice;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("!!!");
-        System.out.println();
+
     }
 }
--- src/main/java/com/bookingservice/models/User.java
+++ src/main/java/com/bookingservice/models/User.java
@@ -1,4 +1,114 @@
 package com.bookingservice.models;
 
 public class User {
+    private int id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String phone;
+    private String gender;
+    private boolean status;
+    private String country;
+
+    public User(int id, String login, String password, String firstName, String lastName, int age, String email, String phone, String gender, boolean status, String country) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.status = status;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
 }
